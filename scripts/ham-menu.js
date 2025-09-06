8242947633f328b9cdc7a81bc2cc6bdc60822bfa
Sarah Lord <113374720+slord30@users.noreply.github.com>
2025-09-06 08:42:47 -0500

    first commit

--- scripts/ham-menu.js
+++ scripts/ham-menu.js
@@ -6,5 +6,4 @@ const navBar = document.querySelector('#nav-bar');
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
     navBar.classList.toggle('show');
-how');
 });
