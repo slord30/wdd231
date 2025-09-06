d3f4d37ce2524a058add0f2200c7b8a16a5c43b9
Sarah Lord <113374720+slord30@users.noreply.github.com>
2025-09-06 08:38:06 -0500

    first commit

--- scripts/ham-menu.js
+++ scripts/ham-menu.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Store the selected elements that we are going to use.
 const navButton = document.querySelector('#ham-btn');
 const navBar = document.querySelector('#nav-bar');
@@ -7,14 +6,5 @@ const navBar = document.querySelector('#nav-bar');
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
     navBar.classList.toggle('show');
-=======
-// Store the selected elements that we are going to use.
-const navButton = document.querySelector('#ham-btn');
-const navBar = document.querySelector('#nav-bar');
-
-// Toggle the show class on and off
-navButton.addEventListener('click', () => {
-    navButton.classList.toggle('show');
-    navBar.classList.toggle('show');
->>>>>>> 125266fe42042289469592f98a15cff65b38ee2d
+how');
 });
