eec47c06593d357239e62c0cd3c83077f5beaafc
slord30 <sarahlord79@gmail.com>
2025-09-11 11:16:46 -0500

    first commit

---
+++ chamber/scripts/members.js
@@ -0,0 +1,46 @@
+const url = "data/members.json";
+const membersContainer = document.querySelector("#members");
+const gridBtn = document.querySelector("#grid-btn");
+const listBtn = document.querySelector("#list-btn");
+
+async function getCompanyData() {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    displayCompanies(data.companies);   
+}
+
+const displayCompanies = (companies) => {
+    membersContainer.innerHTML = ""; //clear old content
+
+    companies.forEach(company => {
+
+        //create a section card
+        let card = document.createElement("section")
+        card.classList.add("member-card");//creating a class name to the <section> element for css. ie <section class ="member-card"></section>
+
+        card.innerHTML = `
+            <h2>${company.name}</h2>
+            <img src="images/${company.image}" alt="logo of ${company.name}" loading="lazy" width="512" height="512>
+            <p><strong>Address:</strong> ${company.address}</p>
+            <p><strong>Phone:</strong> ${company.phone}</p>
+            <p><a href="${company.website}" target="_blank">Visit Website</a></p>
+            <p><strong>Membership Level:</strong> ${company.membership}</p>
+        `;
+
+        membersContainer.appendChild(card);
+    });
+};
+
+//Toggle between the different views
+gridBtn.addEventListener("click", () => {
+    membersContainer.classList.add("grid");
+    membersContainer.classList.remove("list");
+});
+
+listBtn.addEventListener("click", () => {
+    membersContainer.classList.add("list");
+    membersContainer.classList.remove("grid");
+});
+
+getCompanyData();
