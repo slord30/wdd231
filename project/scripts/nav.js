51d69d442dfa464f50a9600a99852cc54583b09f
slord30 <sarahlord79@gmail.com>
2025-09-25 16:01:28 -0500

    first commit

---
+++ project/scripts/nav.js
@@ -0,0 +1,12 @@
+//
+//    Toggle Mobile Navigation
+
+// Store the selected elements that we are going to use. This is not required but a good practice with larger programs where the variable will be referenced more than once.
+const navigation = document.querySelector('.navigation');
+const hamButton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open')
+    hamButton.classList.toggle('open');
+});
